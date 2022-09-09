f51f4ab7e671f5a43935f46e15bd37b8e44804d5
aswann45 <aswann@protonmail.com>
2022-09-09 19:38:51 -0400

    Cleaned up table toolbar

--- src/components/TableToolBar.js
+++ src/components/TableToolBar.js
@@ -9,58 +9,56 @@ function TableToolBar({
   tableInstance, 
   showFilters, 
   setShowFilters, 
-  showColumnTools, 
-  setShowColumnTools, 
   rowSelection, 
   fetchNewQuery, 
   isFetching,
-  nextPageToFetch,
-  totalItems,
-  fetchedItems,
-  backgroundRefreshData,
 }) {
   return (
     <Stack className="TableToolBar" direction='horizontal' gap={2}> 
       
       <Button onClick={() => console.log(rowSelection)}>Log Row Selection</Button>
-      <Button onClick={fetchNewQuery}>Fetch New Query</Button>
+      <Button onClick={fetchNewQuery}>Fetch New Results</Button>
 
       <Dropdown>
-        <Dropdown.Toggle 
-          title='Toggle Visible Columns'
-        >Columns&nbsp;
-          <BsColumns />
+        <Dropdown.Toggle title='Toggle Visible Columns'>
+          
+            <span>Columns&nbsp;</span>
+            <span>
+              <BsColumns />
+            </span>
+          
         </Dropdown.Toggle>
+
         <Dropdown.Menu>
           <ColumnVisibilityToggle tableInstance={tableInstance} />
         </Dropdown.Menu>
+
       </Dropdown>
-            <Button
-        active={false}
-        title='Show/Hide Column Tools'
-        onClick={() => setShowColumnTools(showColumnTools === true ? false : true)}>
-        Column Tools&nbsp;
-        {showColumnTools ?
-          <BsArrowsCollapse /> :
-          <BsArrowsExpand />
-        }
-      </Button>
+
       <Button
         active={false}
         title='Show/Hide Column Filters'
         onClick={() => setShowFilters(showFilters === true ? false : true)}>
-        Filters&nbsp;
+        
         {showFilters ?
-          <BsFilterCircleFill /> :
-          <BsFilterCircle />
+         <Stack direction='horizontal'>
+           <span>Hide Filters&nbsp;</span>
+           <BsFilterCircleFill style={{display: 'block'}}/> 
+         </Stack> :
+         <Stack direction='horizontal'>
+           <span>Show Filters&nbsp;</span>
+           <BsFilterCircle style={{display: 'block'}}/>
+         </Stack>
         }
       </Button>
+
       <Button onClick={() => tableInstance.resetColumnFilters(true)}>
         Clear All Filters
       </Button>
-        {(totalItems && fetchedItems) &&
+      
+        {/*(totalItems && fetchedItems) &&
         <span>Loaded {fetchedItems} of {totalItems}</span>
-        }
+        */}
         {isFetching && 
           <div className='ms-auto' >
             <LoaderSmall />
