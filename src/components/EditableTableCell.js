7b855b9625f18e2a74f584635dc1395deba24fae
aswann45 <aswann@protonmail.com>
2022-09-09 16:40:30 -0400

    Added number and multi-select input options for table edits

--- src/components/EditableTableCell.js
+++ src/components/EditableTableCell.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
 
 function EditableTableCell ({ getValue, row, column, table }) {
 
 //function EditableTableCell ({ props }) {
   const initialValue = getValue();
   const [value, setValue] = useState(initialValue);
+  //const [textBoxHeight, setTextBoxHeight] = useState(42)
 
   const onBlur = () => {
     //console.log('blur')
@@ -22,11 +24,48 @@ function EditableTableCell ({ getValue, row, column, table }) {
   }, [initialValue]);
 
   return (
-    <input 
-      value={value}
-      onChange={onChange}
-      onBlur={onBlur}
-    />
+    
+    <>
+    {column.columnDef.inputType === 'textarea' ? 
+      <Form.Control
+        as={'textarea'}
+        rows={1}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        style={{
+               width: 'inherit',
+               textOverflow: 'ellipsis',
+              }}
+      />
+      :
+        column.columnDef.inputType === 'multi-select' ? 
+          <Form.Select 
+            size='sm'
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+          >
+              <option value='' hidden></option>
+            {column.columnDef.inputValues.map((value) => (
+              <option title={value} value={value} key={value}>{value}</option>
+            ))}
+              <option value={null}>None</option>
+          </Form.Select>
+        :
+          <Form.Control
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+            style={{
+                   width: 'inherit',
+                   overflow: 'hidden',
+                   whiteSpace: 'nowrap',
+                   textOverflow: 'ellipsis',
+                  }}
+          />
+  }
+  </>
   );
 };
 
