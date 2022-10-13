7cc4bdbec532c97b07a06686a0b18349f576a9a4
aswann <103862054+aswann45@users.noreply.github.com>
2022-10-13 18:43:17 -0400

    Fix api login call to get logged in user

--- src/pages/RootPage.js
+++ src/pages/RootPage.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
 import Offcanvas from 'react-bootstrap/Offcanvas'
 
@@ -11,7 +11,7 @@ import MiniDash from '../components/MiniDash';
 function RootPage({isDetail, setIsDetail}) {
 
   const { login } = useUser();
-  const loginResult = useCallback(() => login(), [login]);
+  const loginResult = useMemo(() => login(), []);
   const [showSidebar, setShowSidebar] = useState(false);
   const handleShow = () => setShowSidebar(true);
   const handleClose = () => setShowSidebar(false);
