65468f6d241e4dc08b22736c33a19a34cd131afc
aswann45 <aswann@protonmail.com>
2022-11-21 16:58:36 -0500

    Add disclosure table view and report downloader

--- src/pages/CPFSummaryTables.js
+++ src/pages/CPFSummaryTables.js
@@ -4,8 +4,47 @@ import useMemberFilterOptions from '../hooks/useMemberFilterOptions';
 
 function CPFSummaryTables () {
   const memberOptions = useMemberFilterOptions();
+  const subcommitteeOptions = [
+    {
+      id: 9,
+      name: 'Ag',
+    },
+    {
+      id: 17,
+      name: 'CJS',
+    },
+    {
+      id: 10,
+      name: 'Defense',
+    },
+    {
+      id: 14,
+      name: 'Energy & Water',
+    },
+    {
+      id: 19,
+      name: 'FSGG',
+    },
+    {
+      id: 15,
+      name: 'Homeland',
+    },
+    {
+      id: 16,
+      name: 'MilCon VA',
+    },
+    {
+      id: 13,
+      name: 'Labor HHS',
+    },
+    {
+      id: 18,
+      name: 'THUD',
+    },
+  ]
   const topBarNavLinks = {
     'Member CPF Summaries': '/cpf_summary_tables',
+    'Conference Disclosure Tables': './conference_disclosure_table',
   }
 
   return (
@@ -13,6 +52,7 @@ function CPFSummaryTables () {
       <TopBarNav topBarNavLinks={topBarNavLinks} />
       <Outlet context={[
         memberOptions,
+        subcommitteeOptions
       ]}/>
     </>
   );
